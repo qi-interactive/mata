d31f87be1f98643ded7688e4d2515f358d8ab9d5
Marcin Wiatr <mail.wichura@gmail.com>
2013-05-07 18:25:04 +0100

    Initial commit of Mata Framework
    Signed-off-by: Marcin Wiatr <mail.wichura@gmail.com>

---
+++ controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of DashboardController
+ *
+ * @author wichura
+ */
+class DashboardController extends BaseCMSController {
+   
+    
+    public function actionIndex() {
+        parent::actionIndex();
+        
+    }
+}
+
+?>
