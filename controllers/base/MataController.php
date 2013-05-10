5ca0a4a4b95b6b85fc420ad20b4b9fdbcb9060a8
Marcin Wiatr <mail.wichura@gmail.com>
2013-05-10 23:15:24 +0100

    pulled up assets
    Signed-off-by: Marcin Wiatr <mail.wichura@gmail.com>

--- controllers/base/MataController.php
+++ controllers/base/MataController.php
@@ -11,8 +11,20 @@
  * @author wichura
  */
 class MataController extends BaseAuthorizedController {
-      public $layout = "mainWithMenu";
-      
+
+    public $layout = "mainWithMenu";
+    public $mataAssetUrl;
+
+    public function filterBeforeExec($filterChain) {
+
+        $this->mataAssetUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('mata') . DIRECTORY_SEPARATOR . "assets", false, -1, YII_DEBUG);
+        $cs = Yii::app()->getClientScript();
+        $cs->registerCssFile($this->mataAssetUrl . '/css/reset.css');
+        $cs->registerCssFile($this->mataAssetUrl . '/css/layout.css');
+
+        parent::filterBeforeExec($filterChain);
+    }
+
 }
 
 ?>
