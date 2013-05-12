f46099ef79ffe279e68e7b0d924e4010ea857586
Marcin Wiatr <mail.wichura@gmail.com>
2013-05-12 09:35:47 +0100

    Updated the way menu items get rendered
    Signed-off-by: Marcin Wiatr <mail.wichura@gmail.com>

--- views/layouts/mainWithMenu.php
+++ views/layouts/mainWithMenu.php
@@ -10,39 +10,22 @@
                 if (count($moduleGroup->modules) == 1 && count(Yii::app()->getModule($moduleGroup->modules[0]->Name)->getNav()) == 1):
                     ?>
                     <li><a href="<?php echo current(Yii::app()->getModule($moduleGroup->modules[0]->Name)->getNav()) ?>"><img src="<?php echo $moduleGroup->IconPath ?>" /></a></li>
-
                     <?php
                     continue;
                 endif;
                 ?>
 
                 <li><a href="javascript:void(0)" data-sub-nav="<?php echo strtolower($moduleGroup->Name) ?>" ><img src="<?php echo $moduleGroup->IconPath ?>" /></a></li>
-                <div id="sub-menu-<?php echo strtolower($moduleGroup->Name) ?>" class="sub-menu">
-                    <h2>Accounts</h2>
-                    <p>Lorem ipsum dolor sit amet, consectuter adupiscig dig.</p>
-                    <ul>
-                        <?php foreach ($moduleGroup->modules as $module): ?>
-
-
-                            <?php
-                            foreach (Yii::app()->getModule($module->Name)->getNav() as $label => $url):
-                                $assetURL = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias($module->Name) . DIRECTORY_SEPARATOR . "assets", false, -1, YII_DEBUG);
-                                echo "<li><a href='$url'>" .
-                                CHtml::image($assetURL . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "small-icon.png") .
-                                $label .
-                                "</a></li>";
-                            endforeach;
-                            ?>
-                            <?php
-                        endforeach;
-                        ?>
-                    </ul>
-                </div>
                 <?php
             endforeach;
             ?>
-        </ul>
 
+            <footer>
+                <a id="project-name" href='javascript:void(0)' onclick='mata.switchProject()'><?php echo $this->user->project->Name ?></a>
+                <a href="/user/logout">You are <?php echo $this->user->FirstName . " " . $this->user->LastName ?></a>
+            </footer>
+        </ul>
+    </div>
 
 <!--            <li id="side-menu-dashboard"><a onclick="hideSubmenu()" href='/mata/dashboard'><img src='/images/layout/icons/map-icon.png' /></a></li>
             <li id="side-menu-content"><a href='javascript:void(0)' onclick="showSideMenu('content')"><img src='/images/layout/icons/note-icon.png' /></a></li>
@@ -52,11 +35,33 @@
             <li id="side-menu-help"><a href='#'><img src='/images/layout/icons/loudspeaker-icon.png' /></a></li>
         </ul>-->
 
-        <footer>
-            <a id="project-name" href='javascript:void(0)' onclick='mata.switchProject()'><?php echo $this->user->project->Name ?></a>
-            <a href="/user/logout">You are <?php echo $this->user->FirstName . " " . $this->user->LastName ?></a>
-        </footer>
-    </div>
+    <?php foreach (MataModuleGroup::model()->with("modules")->findAll() as $moduleGroup): ?>
+        <div id="sub-menu-<?php echo strtolower($moduleGroup->Name) ?>" class="sub-menu">
+            <h2>Accounts</h2>
+            <p>Lorem ipsum dolor sit amet, consectuter adupiscig dig.</p>
+            <ul>
+                <?php foreach ($moduleGroup->modules as $module): ?>
+
+
+                    <?php
+                    foreach (Yii::app()->getModule($module->Name)->getNav() as $label => $url):
+                        $assetURL = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias($module->Name) . DIRECTORY_SEPARATOR . "assets", false, -1, YII_DEBUG);
+                        echo "<li><a href='$url'>" .
+                        CHtml::image($assetURL . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . "small-icon.png") .
+                        $label .
+                        "</a></li>";
+                    endforeach;
+                    ?>
+                    <?php
+                endforeach;
+                ?>
+            </ul>
+        </div>
+        <?php
+    endforeach;
+    ?>
+
+
 </div>
 
 <iframe frameborder="0" id='content-container'>
@@ -64,70 +69,70 @@
 
 <script>
 
-                $(window).ready(function() {
-                    $("#side-menu a").first().trigger("click");
-                    // Requires jQuery!
+            $(window).ready(function() {
+                $("#side-menu a").first().trigger("click");
+                // Requires jQuery!
 
-                    jQuery.ajax({
-                        url: "http://jira.qi-interactive.com/s/en_UKevdmcy-418945332/812/4/1.2.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?collectorId=c33a832b",
-                        type: "get",
-                        cache: true,
-                        dataType: "script"
-                    });
+                jQuery.ajax({
+                    url: "http://jira.qi-interactive.com/s/en_UKevdmcy-418945332/812/4/1.2.7/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?collectorId=c33a832b",
+                    type: "get",
+                    cache: true,
+                    dataType: "script"
+                });
 
-                })
-                $("#side-menu-container").find("li a").bind("click", function() {
-
-
-                    $("#content-container").attr("src", $(this).attr("href"));
-                    $(this).parents("ul").first().find(".active").removeClass("active")
-                    $(this).parent().first().addClass("active");
+            })
+            $("#side-menu-container").find("li a").bind("click", function() {
 
 
-                    if ($(this).parents(".sub-menu").length == 0)
-                         hideSubmenu()
-                     
-                    if ($(this).attr("data-sub-nav") != null) {
-                        showSideMenu($(this).attr("data-sub-nav"))
+                $("#content-container").attr("src", $(this).attr("href"));
+                $(this).parents("ul").first().find(".active").removeClass("active")
+                $(this).parent().first().addClass("active");
+
+
+                if ($(this).parents(".sub-menu").length == 0)
+                    hideSubmenu()
+
+                if ($(this).attr("data-sub-nav") != null) {
+                    showSideMenu($(this).attr("data-sub-nav"))
+                }
+
+                return false;
+            })
+
+            function showSideMenu(section) {
+                hideSubmenu()
+                $("#sub-menu-" + section).addClass("active").transition({
+                    left: 100
+                });
+
+                $("#sub-menu-" + section).show();
+
+                $("#side-menu-" + section).addClass("active")
+
+                $("#content-container").transition({
+                    "margin-left": 321
+                });
+
+                $(window).bind("keyup.sub-menu", function(e) {
+                    if (e.keyCode == 27) {
+                        hideSubmenu();
+                        e.stopPropagation();
                     }
 
-                    return false;
-                })
+                });
+            }
 
-                function showSideMenu(section) {
-                    hideSubmenu()
-                    $("#sub-menu-" + section).addClass("active").css({
-                        left: 100
-                    });
+            function hideSubmenu() {
+                $(".sub-menu.active").transition({
+                    left: -121
+                }).removeClass("active")
 
-                    $("#sub-menu-" + section).show();
+                $("#content-container").transition({
+                    "margin-left": 100
+                });
 
-                    $("#side-menu-" + section).addClass("active")
-
-                    $("#content-container").transition({
-                        "margin-left": 321
-                    });
-
-                    $(window).bind("keyup.sub-menu", function(e) {
-                        if (e.keyCode == 27) {
-                            hideSubmenu();
-                            e.stopPropagation();
-                        }
-
-                    });
-                }
-
-                function hideSubmenu() {
-                    $(".sub-menu.active").transition({
-                        left: -121
-                    }).removeClass("active")
-
-                    $("#content-container").transition({
-                        "margin-left": 100
-                    });
-
-                    $(window).unbind("keyup.sub-menu");
-                }
+                $(window).unbind("keyup.sub-menu");
+            }
 </script>
 
 <?php $this->endContent(); ?>
